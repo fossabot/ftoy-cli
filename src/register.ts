1dda42b22e41e5a074cafa00f1c9cc36a2cbc837
chenshihao <victor_csh@outlook.com>
2018-10-28 21:35:49 +0800

    rm bundle & test

--- src/register.ts
+++ src/register.ts
@@ -9,16 +9,18 @@ class Register {
   }
 
   public bootstrap() {
-    const cli = yargs
+    const cli: yargs.Argv = yargs
       .commandDir(this.commandDir)
       .strict()
       .version()
       .help()
       .showHelpOnFail(false, "可使用 help 选项查看帮助")
       .epilogue(
-        "了解更多信息，请前往 https://github.com/ChenShihao/ftoy-cli.git",
+        "了解更多信息，请前往 https://github.com/ChenShihao/ftoy-cli.git"
       );
-
+    Object.assign(cli, {
+      $0: "ftoy"
+    });
     if (!cli.argv._.length) {
       cli.showHelp();
     }
